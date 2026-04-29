9451968d86e74cb720517b02ef2b889774593978
H1de0Fern4ndes <isaac.hideo.fernandes@gmail.com>
2026-04-29 11:41:17 -0300

    htdocs

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ function somar(a, b) {
 function mensagemBoasVindas(nome) {
   return `Olá, ${nome}!`;
 }
+function verificarMaior(a,b){
+  if(a>b){
+    return a
+  }
+  if(a<b) {
+    return b
+  }
+  return "iguais"
+  
+}
+
 
 module.exports = { somar, mensagemBoasVindas };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const { somar, mensagemBoasVindas } = require('./script');
+const { somar, mensagemBoasVindas, verificarMaior } = require('./script');
 
 test('soma dois números', () => {
   expect(somar(2, 3)).toBe(5);
@@ -6,4 +6,7 @@ test('soma dois números', () => {
 
 test('mensagem de boas vindas', () => {
   expect(mensagemBoasVindas('Diogo')).toBe('Olá, Diogo!');
+});
+test('Verificar maior numero', () => {
+  expect(verificarMaior(1,2)).toBe(2);
 });
